e66b8f7ae1c059a0b0c4855e89bc5e27e2e9e0fb
bluron <julei_me@163.com>
2021-03-24 09:31:41 +0800

    树形控件封装

---
+++ src/components/TreePlus.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-tree
+    :data="treeData"
+    ref="tree"
+    show-checkbox
+    default-expand-all
+    node-key="id"
+    highlight-current
+    :props="defaultProps">
+  </el-tree>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      checkedKeys: [],
+      defaultProps: {
+        label: 'label',
+        children: 'children'
+      }
+    }
+  },
+  methods: {
+    setCheckedKeys (data) {
+      this.$refs.tree.setCheckedKeys(data)
+    },
+    getCheckedKeys () {
+      return this.$refs.tree.getCheckedKeys()
+    },
+    getHalfCheckedKeys () {
+      return this.$refs.tree.getHalfCheckedKeys()
+    }
+  },
+  props: {
+    treeData: Array
+  }
+}
+</script>
