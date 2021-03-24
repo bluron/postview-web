e6d0afe384b1d4d5d3a3907ba26c7c46eb39acc1
bluron <julei_me@163.com>
2021-03-24 14:32:40 +0800

    路由管理

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,63 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import store from '../store'
+import Home from '../views/layout/Home.vue'
+import { addDynamicMenuRoutes } from '../api/layout/index.js'
+import { navMenu } from '../api/menu/index.js'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    component: Home,
+    children: [{
+      path: 'dashboard',
+      component: () => import('../views/dashboard/Dashboard.vue'),
+      meta: {
+        title: '首页',
+        icon: 'el-icon-info'
+      }
+    }]
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/login/Login.vue')
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('pvToken')
+  if (to.path === '/login') {
+    if (token !== null) {
+      next({ path: '/' })
+    } else {
+      next()
+    }
+  } else {
+    if (token === null) {
+      next({ path: '/login' })
+    } else {
+      if (store.state.navMenu.length === 0) {
+        navMenu().then(res => {
+          if (res !== undefined) {
+            store.commit({
+              type: 'navMenu',
+              navMenu: res
+            })
+            const routers = addDynamicMenuRoutes(res)
+            router.addRoutes(routers)
+          }
+        })
+      }
+      next()
+    }
+  }
 })
 
 export default router
