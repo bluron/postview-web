0934005472dd8e4007d97e5758eba2fcfcaa125b
bluron <julei_me@163.com>
2021-03-24 14:32:28 +0800

    状态管理

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,28 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    openedTab: [],
+    activatedTab: {},
+    navMenu: []
   },
   mutations: {
+    openTab (state, payload) {
+      if (payload.option === 'add') {
+        state.openedTab.push(payload.openedTab)
+      } else {
+        state.openedTab.map((v, i) => {
+          if (v.name === payload.name) {
+            state.openedTab.splice(i, 1)
+          }
+        })
+      }
+    },
+    activeTab (state, payload) {
+      state.activatedTab = payload.activatedTab
+    },
+    navMenu (state, payload) {
+      state.navMenu = payload.navMenu
+    }
   },
   actions: {
   },
