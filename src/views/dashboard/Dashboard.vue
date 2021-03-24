7533eb74424534d604ea12e334c08671aff7c533
bluron <julei_me@163.com>
2021-03-24 14:45:03 +0800

    仪表盘

---
+++ src/views/dashboard/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-carousel :interval="5000" type="card" height="500px">
+    <el-carousel-item class="el-carousel__item-1">
+    </el-carousel-item>
+    <el-carousel-item class="el-carousel__item-2">
+    </el-carousel-item>
+    <el-carousel-item class="el-carousel__item-3">
+    </el-carousel-item>
+  </el-carousel>
+</template>
+
+<style>
+  .el-carousel__item-1 {
+    background-color: #b95e5e;
+    border-radius: 10px;
+  }
+  .el-carousel__item-2 {
+    background-color: #52c578;
+    border-radius: 10px;
+  }
+  .el-carousel__item-3 {
+    background-color: #41a7b9;
+    border-radius: 10px;
+  }
+</style>
