b118c28f067e7230ad9e7bcb211aa0fb77d8f5e6
bluron <julei_me@163.com>
2021-03-24 09:32:16 +0800

    对话框封装

---
+++ src/components/DialogPlus.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-dialog
+    :visible.sync="dialogVisible"
+    :show="show"
+    @close="$emit('update:show', false)">
+    <span slot="title"><slot name="title"></slot></span>
+    <slot name="content"></slot>
+    <span slot="footer" class="dialog-footer">
+      <el-button type="primary" @click="dialogVisible = false">取 消</el-button>
+      <el-button type="primary" @click="sure">确 定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      dialogVisible: this.show
+    }
+  },
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    sure () {
+      this.$emit('submit')
+    }
+  },
+  watch: {
+    show (val) {
+      this.dialogVisible = this.show
+    }
+  }
+}
+</script>
